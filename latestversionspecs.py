3469af84f9cb87b516ce009960fce8dccf33aaa8
Amrita Kohli <amritakohli@microsoft.com>
2024-06-13 11:06:39 -0700

    results of packages to address

--- latestversionspecs.py
+++ latestversionspecs.py
@@ -2,6 +2,7 @@
  
 import subprocess
 import json
+import time
  
 def get_latest_version(package_name):
     try:
@@ -13,7 +14,9 @@ def get_latest_version(package_name):
        
         if result.stdout != "":
             version = result.stdout.strip()
-
+        if result.stderr != "":
+            version = result.stderr.strip()
+        # print(version)
         return f"{package_name}: {version}"
         # TODO: add to a file instead of print
     except subprocess.CalledProcessError as e:
@@ -23,24 +26,43 @@ def get_latest_version(package_name):
  
 if __name__ == "__main__":
     # for everypackage in cgmanifest.json:
-    filename = "./cgmanifest.json"
+    # filename = "./cgmanifest.json"
 
     # Load the JSON data from a file
-    with open(filename, 'r') as file:
-        data = json.load(file)
+    # with open(filename, 'r') as file:
+    #     data = json.load(file)
+    start_time = time.time()
 
-    # Open the output file in append mode
-    with open('output.txt', 'a') as output_file:
-        # Iterate through the registrations and write the package names to the output file
-        for registration in data["Registrations"]:
-            component = registration.get("component", {})
-            other = component.get("other", {})
-            name = other.get("name")
-            if name:
-                # get_latest_version(name)
-                # break
-                output_file.write(get_latest_version(name) + '\n')
-                # break
+    input_file = "packagesToAddress.txt"
+    output_file = "outputAddressed.txt"
+
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for name in infile:
+            name = name.strip()
+            result = get_latest_version(name)
+            print(result)
+            print('\n')
+            print(time.time() - start_time)
+            print('\n')
+            outfile.write(result + '\n')
+            outfile.flush() # write output to file immediately
+    
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Total exec time: {elapsed_time} seconds")
+
+    # # Open the output file in append mode
+    # with open('output.txt', 'a') as output_file:
+    #     # Iterate through the registrations and write the package names to the output file
+    #     for line in filename:
+    #         component = registration.get("component", {})
+    #         other = component.get("other", {})
+    #         name = other.get("name")
+    #         if name:
+    #             # get_latest_version(name)
+    #             # break
+    #             output_file.write(get_latest_version(name) + '\n')
+    #             # break
     
 
-        # get_latest_version("ansible")
+    #     # get_latest_version("ansible")
